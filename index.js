1cc9d1c5dd7d74c0bfaa19c1192da8f7aa6c5b5b
Kevin Huang <yellow.khaos@gmail.com>
2018-10-08 14:51:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function Scooter (year, color, model) {
 function Driver (name, age, experience) {
   this.name = name
   this.age = age
-  this.experience experience
+  this.experience = experience
 }
